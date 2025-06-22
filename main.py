b960491f3c5b2dc95692cff0e1f3ee75c66383d6
Diego <diegoo9997@gmail.com>
2025-06-21 23:05:55 -0600

    Terminamos

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
-from stats import get_word_count,  get_chars, sorted_dicts
+from stats import get_word_count, sorted_dicts
+import sys
+
+if len(sys.argv)!=2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+
 
 def main():
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}...")
     print("----------- Word Count ----------")
-    print(f"Found {get_word_count('books/frankenstein.txt')} total words") 
+    print(f"Found {get_word_count(sys.argv[1])} total words") 
     print("--------- Character Count -------")
-    for item in sorted_dicts('books/frankenstein.txt'):
+    for item in sorted_dicts(sys.argv[1]):
         if item['char'].isalpha():
             print(f"{item['char']}: {item['num']}")
 
